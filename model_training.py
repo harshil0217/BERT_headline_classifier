a81e3b5ad1a4557d29337abfb580c1d2f31d3643
harshil0217 <harshil0217@gmail.com>
2024-07-13 17:43:58 -0400

    built classifier

--- model_training.py
+++ model_training.py
@@ -1,2 +1,27 @@
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_text as text
+import keras_nlp
+
+#load encoded and preprocess model
+tfhub_handle_encoder = 'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-4_H-512_A-8/1'
+tfhub_handle_preprocess = 'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3'
+
+#build classifier
+
+def build_classifier_model():
+    text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
+    preprocessor = keras_nlp.models.BertPreprocessor.from_preset("bert_base_en_uncased",trainable=True)
+    encoder_inputs = preprocessor(text_input)
+    encoder = keras_nlp.models.BertBackbone.from_preset("bert_base_en_uncased")
+    outputs = encoder(encoder_inputs)
+    net = outputs['pooled_output']
+    net = tf.keras.layers.Dropout(0.1)(net)
+    net = tf.keras.layers.Dense(1, activation=None, name='classifier')(net)
+    return tf.keras.Model(text_input, net)
+
+classifier_model = build_classifier_model()
+bert_raw_result = classifier_model(tf.constant(["poopity_scoop"]))
+print(tf.sigmoid(bert_raw_result))
 
 
