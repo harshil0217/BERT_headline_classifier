ddf7ff55dc56867f1b6efc7a8fc9482f6ec6c872
harshil0217 <harshil0217@gmail.com>
2024-07-17 16:54:20 -0400

    created pytorch variant for model training

--- model_training.py
+++ model_training.py
@@ -14,7 +14,7 @@ tfhub_handle_preprocess = 'https://tfhub.dev/tensorflow/bert_en_uncased_preproce
 
 def build_classifier_model():
     text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
-    preprocessor = keras_nlp.models.BertPreprocessor.from_preset("bert_base_en_uncased",trainable=True)
+    preprocessor = keras_nlp.models.BertPreprocessor.from_preset("bert_base_en_uncased", trainable=True)
     encoder_inputs = preprocessor(text_input)
     encoder = keras_nlp.models.BertBackbone.from_preset("bert_base_en_uncased")
     outputs = encoder(encoder_inputs)
@@ -25,7 +25,7 @@ def build_classifier_model():
 
 
 loss = tf.keras.losses.BinaryCrossentropy(from_logits=True)
-metrics = tf.metrics.BinaryAccuracy()
+metrics = [tf.metrics.BinaryAccuracy()]
 
 #load in the datasets
 with open('data/X_train.pkl', 'rb') as data:
@@ -41,18 +41,18 @@ with open('data/y_test.pkl', 'rb') as data:
     y_test = pickle.load(data)
 
 #train the model
-epochs = 5
-steps_per_epoch = len(X_train)//32
-num_train_steps = steps_per_epoch * epochs
-num_warmup_steps = int(0.1*num_train_steps)
+
+loss = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+metrics = [tf.metrics.BinaryAccuracy()]
+
 
 init_lr = 3e-5
-optimizer = optimization.create_optimizer(init_lr=init_lr,
-                                          num_train_steps=num_train_steps,
-                                          num_warmup_steps=num_warmup_steps,
-                                          optimizer_type='adamw')
+optimizer = tf.keras.optimizers.Adam(learning_rate=init_lr)
 
+classifier_model = build_classifier_model()
 
+classifier_model.compile(optimizer=optimizer,
+                         loss=loss,
+                         metrics=metrics)
 
-
-
+history = classifier_model.fit(x=X_train, y=y_train, validation_data=(X_test, y_test), epochs=5)
